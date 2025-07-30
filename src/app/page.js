a18ab9dd989c82143f24dd075e81f510919d7c2a
afdol-LN <afdolleenud63@gmail.com>
2025-07-30 07:00:25 +0700

    30/7/68

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Todolist from "./Todolist.js";
+import Snowball from "./SnowBall.js";
 export default function Home() {
   return (
     <>
-      
-      <Todolist/>
+      <Snowball/>
+      {/* <Todolist/> */}
     </>
   );
 }
